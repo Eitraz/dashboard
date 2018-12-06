c609a0db89ea6cc0f8a10a872e866b2b71fb053d
Petter Alstermark <petter.alstermark@entiros.se>
2018-12-06 15:36:59 +0100

    Weather on dashboard view

--- src/main/java/com/eitraz/dashboard/view/DashboardView.java
+++ src/main/java/com/eitraz/dashboard/view/DashboardView.java
@@ -2,10 +2,12 @@ package com.eitraz.dashboard.view;
 
 import com.eitraz.dashboard.Constants;
 import com.eitraz.dashboard.MainLayout;
+import com.eitraz.dashboard.service.DarkSkyService;
 import com.eitraz.dashboard.service.GoogleCalendarService;
 import com.eitraz.dashboard.service.TemperatureService;
 import com.eitraz.dashboard.view.component.calendar.CalendarCard;
 import com.eitraz.dashboard.view.component.temperature.TemperaturesCard;
+import com.eitraz.dashboard.view.component.weather.WeatherCard;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +16,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Route(value = Constants.PAGE_ROOT, layout = MainLayout.class)
 public class DashboardView extends FlexLayout {
     @Autowired
-    public DashboardView(TemperatureService temperatureService, GoogleCalendarService googleCalendar) {
+    public DashboardView(TemperatureService temperatureService, GoogleCalendarService googleCalendar, DarkSkyService darkSkyService) {
         setClassName("dashboard");
 
         add(new TemperaturesCard(temperatureService));
-        add(new CalendarCard(googleCalendar, 10));
+
+        FlexLayout columns = new FlexLayout(
+                new CalendarCard(googleCalendar, 10),
+                new WeatherCard(darkSkyService));
+        columns.addClassName("columns");
+        add(columns);
     }
 }
--- src/main/java/com/eitraz/dashboard/view/WeatherView.java
+++ src/main/java/com/eitraz/dashboard/view/WeatherView.java
@@ -5,12 +5,10 @@ import com.eitraz.dashboard.MainLayout;
 import com.eitraz.dashboard.component.Div;
 import com.eitraz.dashboard.service.DarkSkyService;
 import com.eitraz.dashboard.view.component.weather.WeatherCard;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SuppressWarnings("SpringJavaAutowiredMembersInspection")
-@StyleSheet("frontend://styles/weather.css")
 @Route(value = Constants.PAGE_WEATHER, layout = MainLayout.class)
 public class WeatherView extends Div {
 
