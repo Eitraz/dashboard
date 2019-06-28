61d73cb069638b9f8e51c3368bc5e4551d129680
Petter Alstermark <git@eitraz.com>
2019-06-28 17:20:03 +0200

    Security

---
+++ src/main/java/com/eitraz/dashboard/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.eitraz.dashboard;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${server.username")
+    private String username;
+
+    @Value("${server.password")
+    private String password;
+
+    private final CustomAuthenticationEntryPoint entryPoint;
+
+    public SecurityConfig(CustomAuthenticationEntryPoint entryPoint) {
+        this.entryPoint = entryPoint;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(entryPoint);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(username)
+                .password("{noop}" + password)
+                .roles("USER");
+    }
+}
